068e2220865c87a27e59b5bbd3606af304f26107
RingoTangs <1466637477@qq.com>
2023-12-22 19:49:02 +0800

    feat: update

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent, toRefs, unref } from 'vue'
+import type { PropType } from 'vue'
 import './Button.css'
 
 export default defineComponent({
-  setup() {
-    // Hello
-    return () => <h1 class="btn">Hello VueJsx</h1>
+  name: 'Button',
+  inheritAttrs: false,
+  props: {
+    name: {
+      type: String as PropType<string>,
+      default: '',
+    },
+  },
+  setup(props, { attrs }) {
+    console.log(props)
+    const { name } = toRefs(props)
+    return () => (
+      <>
+        <h1 class="btn" {...attrs}>
+          Hello VueJsx {unref(name)}
+        </h1>
+      </>
+    )
   },
 })
